aa744fefbd84d7b805f4d50ef4afda9d366a51ed
nayab jahan <nayabzafir@gmail.com>
2022-08-23 21:25:55 +0530

    Added:LinkedList

--- src/main/java/com/blz/LinkedList/LinkedListMain.java
+++ src/main/java/com/blz/LinkedList/LinkedListMain.java
@@ -2,12 +2,7 @@ package com.blz.LinkedList;
 
 public class LinkedListMain {
     public static void main(String[] args) {
-        MyNode<Integer> myFirstNode = new MyNode<>(56);
-        MyNode<Integer> mySecondNode = new MyNode<>(30);
-        MyNode<Integer> myThirdNode = new MyNode<>(70);
-        myFirstNode.setNext(mySecondNode);
-        mySecondNode.setNext(myThirdNode);
-        System.out.println(myFirstNode.getKey()+"->" +mySecondNode.getKey()+"->"+ myThirdNode.getKey() + "nodes");
+        System.out.println("Linked List Operations");
 
     }
 }
